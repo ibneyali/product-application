bf8575d7ea05aeca21c72b0323cc9aea1ee2284e
ibneyali <ibneyalimbd@gmail.com>
2024-04-11 15:25:46 +0530

    Add new product application with spring boot and reactjs

---
+++ product/src/main/java/com/ibn/product/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.ibn.product.controller;
+
+import com.ibn.product.model.Product;
+import com.ibn.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateById(id, product);
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+}
